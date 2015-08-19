29a8014eca689d3c56e17a0a9bf501621ba74fd4
naomiajacobs <naomiajacobs@gmail.com>
2015-08-19 12:38:19 -0700

    Made functional stack tests pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,56 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+
+    var sizeNow = someInstance.size();
+
+    for (var i = sizeNow; i > 0; i--) {
+      storage[i] = storage[i-1];
+    }
+
+    storage[0] = value;
+
   };
 
   someInstance.pop = function(){
+
+    var sizeNow = someInstance.size();
+
+    if (sizeNow === 0) {
+      return;
+    }
+
+    var result = storage[0];
+
+    if (sizeNow === 1) {
+      delete storage[0];
+    }
+
+    for (var i = 1; i < sizeNow; i++) {
+
+      storage[i-1] = storage[i];
+      if (i === sizeNow-1) {
+        delete storage[i];
+      }
+
+    }
+
+    return result;
+
   };
 
   someInstance.size = function(){
+
+    // var count = 0;
+
+    // _.each(storage, function(item) {
+    //   count++;
+    // });
+
+    // return count;
+
+    return Object.keys(storage).length;
+
   };
 
   return someInstance;
