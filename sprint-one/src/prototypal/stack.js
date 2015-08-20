cda5e1ed7fa24e24541f29fbf3976e09414c6fab
naomiajacobs <naomiajacobs@gmail.com>
2015-08-20 11:31:18 -0700

    Make prototypal stack tests pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(value) {
 
+    var currentSize = this.size();
+
+    this.storage[currentSize] = value;
+
+    return currentSize;
+  },
+
+  pop: function() {
+    var currentSize = this.size();
+
+    var popValue = this.storage[currentSize-1];
+
+    delete this.storage[currentSize-1];
+
+    return popValue;
+  },
+
+  size: function(count) {
+
+    return Object.keys(this.storage).length;
+
+  }
+};
