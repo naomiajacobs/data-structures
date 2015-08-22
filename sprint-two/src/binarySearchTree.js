e8921ad3cb36aef9daae9b72c42da866b2023f86
naomiajacobs <naomiajacobs@gmail.com>
2015-08-22 10:12:07 -0700

    Make all tests pass

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,65 @@
 var BinarySearchTree = function(value){
+
+  var newTree = Object.create(BinarySearchTree.prototype);
+
+  newTree.value = value;
+
+  newTree.left = null;
+
+  newTree.right = null;
+
+  return newTree;
+
 };
 
+BinarySearchTree.prototype = {};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+BinarySearchTree.prototype.insert = function (value) {
+
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+
+  } else if (value >= this.value) {
+    if (this.right == null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+
+  } else {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(func) {
+  func(this.value);
+
+  if (this.left !== null) {
+    this.left.depthFirstLog(func);
+  } else if (this.right !== null) {
+    this.right.depthFirstLog(func);
+  }
+};
